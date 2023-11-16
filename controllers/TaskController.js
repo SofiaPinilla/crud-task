eb0b308c496b6dcb156254dbe03389c463b29162
Demis Previotto <demis.previotto@gmail.com>
2023-11-16 11:56:01 +0100

    complete task

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -15,23 +15,36 @@ const TaskController = {
 				.send({ msg: "There was an error trying to create the task", error });
 		}
 	},
-	async getAll (req,res) {
+	async getAll(req, res) {
 		try {
 			const tasks = await Task.find()
 			res.status(200).send(tasks)
 		} catch (error) {
 			console.error(error)
-			res.status(500).send({msg:"Error trying to get all tasks", error})
+			res.status(500).send({ msg: "Error trying to get all tasks", error })
 		}
 	},
-	async getById (req,res) {
+	async getById(req, res) {
 		try {
 			const task = await Task.findById(req.params._id)
 			res.status(200).send(task)
 		} catch (error) {
 			console.error(error)
-			res.status(500).send({msg:"Error trying to get a task with Id", error})
+			res.status(500).send({ msg: "Error trying to get a task with Id", error })
 		}
 	},
+	async complete(req, res) {
+		try {
+			let task = await Task.findByIdAndUpdate(
+				req.params._id,
+				{ completed: true },
+				{ new: true }
+			)
+			res.status(200).send({ msg: "task completed", task })
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ msg: "There was a problem trying to complete the task" })
+		}
+	}
 };
 module.exports = TaskController;
--- routes/tasks.js
+++ routes/tasks.js
@@ -5,5 +5,6 @@ const TaskController = require("../controllers/TaskController");
 router.post("/", TaskController.create);
 router.get("/", TaskController.getAll);
 router.get("/:_id", TaskController.getById);
+router.put("/:_id", TaskController.complete);
 
 module.exports = router;

083122fecd7ffa59a9806cc5fd1f41745fc55443
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-16 10:01:11 +0100

    main structure

---
+++ config/config.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI;
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Database connected succesfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error connecting with the database");
+  }
+};
+module.exports = {
+  dbConnection,
+};

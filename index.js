73d875cc49628480209d4823f0ba6f9e720be0fc
SofiaPinilla <contact@sofiapinilla.com>
2023-11-17 10:08:54 +0100

    docs with swagger added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const swaggerUI = require('swagger-ui-express')
+const docs = require('./docs/index')
 
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +10,8 @@ const { dbConnection } = require("./config/config");
 app.use(express.json());
 
 app.use("/tasks", require("./routes/tasks"));
+
+app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs))
 
 dbConnection();
 
